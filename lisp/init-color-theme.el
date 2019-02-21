ef8a8575c16409879699d3a24175e6309d6e156f
liaozaixue <liaozaixue@jd.com>
2019-02-21 10:21:38 +0800

    add rust support

---
+++ lisp/init-color-theme.el
@@ -0,0 +1,7 @@
+;; -*- coding: utf-8; lexical-binding: t; -*-
+
+;; Don't ask before rereading the TAGS files if they have changed
+(color-theme-initialize)
+(color-theme-gnome2)
+
+(provide 'init-color-theme)
